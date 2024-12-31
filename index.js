61d3960e921b7a356f203c059bf6bfe36727abb0
myKollege <mykollegeindia@gmail.com>
2024-12-31 11:44:08 +0600

    cors problem fiexed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+const cors = require("cors"); // Import the CORS middleware
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
 // Constants
 const PORT = process.env.PORT || 3000;
